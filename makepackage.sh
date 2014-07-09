ffbab29c508037da732ff6b4c7e389f5fd092961
Nick Badal <nick@ouya.tv>
2014-07-09 16:47:24 -0700

    Change distributed package name

--- makepackage.sh
+++ makepackage.sh
@@ -2,8 +2,8 @@
 pushd . > /dev/null
 
 cd project/
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath `pwd` -exportPackage Assets `ls ProjectSettings/*` dist.unitypackage -logFile output.log -quit
-mv dist.unitypackage ..
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -projectPath `pwd` -exportPackage Assets `ls ProjectSettings/*` ouya-unity-2d-demo.unitypackage -logFile output.log -quit
+mv ouya-unity-2d-demo.unitypackage ..
 
 popd > /dev/null
 
